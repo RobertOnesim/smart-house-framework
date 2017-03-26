26d48f5cbfc72f2219d85c295fb91372c041298e
Robert Onesim <robert.onesim@gmail.com>
2017-03-26 18:19:04 +0300

    Added structure

---
+++ framework/src/main/java/com/ronesim/SmartHouseFrameworkApplication.java
@@ -0,0 +1,12 @@
+package com.ronesim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHouseFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHouseFrameworkApplication.class, args);
+	}
+}
---
+++ framework/src/test/java/com/ronesim/SmartHouseFrameworkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ronesim;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartHouseFrameworkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
