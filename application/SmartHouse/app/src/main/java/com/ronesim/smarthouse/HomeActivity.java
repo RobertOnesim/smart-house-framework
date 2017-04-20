44d5fb3c5a6a9e76dc7f1e1dc758f52b574c07cd
Robert Onesim <robert.onesim@gmail.com>
2017-04-20 23:05:07 +0300

    Added toolbar and menu.

--- application/SmartHouse/app/src/main/java/com/ronesim/smarthouse/HomeActivity.java
+++ application/SmartHouse/app/src/main/java/com/ronesim/smarthouse/HomeActivity.java
@@ -2,11 +2,23 @@ package com.ronesim.smarthouse;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.ronesim.smarthouse.account.LoginActivity;
 
 public class HomeActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +28,66 @@ public class HomeActivity extends AppCompatActivity {
         // Login system
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+
+        // Adding Toolbar to Main screen
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Create Navigation drawer and inflate layout
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+
+        // Adding menu icon to Toolbar
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white);
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        // Set behavior of navigation drawer
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    // This method will trigger on item Click of navigation menu
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        // Set item in checked state
+                        menuItem.setChecked(true);
+                        // TODO ronesim: handle navigation
+                        // Closing drawer on item click
+                        drawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+
+        // Adding Floating Action Button to bottom right of main view
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO ronesim handle button
+                System.out.println("pressed");
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        // HAndle item selection
+        switch (menuItem.getItemId()) {
+            case R.id.action_settings:
+                System.out.println("Hit Settings");
+                return true;
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+            default:
+                return super.onOptionsItemSelected(menuItem);
+        }
     }
 }
